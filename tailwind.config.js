a5d6f892073c45ebaab6c35d70db1dd895417b96
sandysormin19 <sandysormin9@gmail.com>
2025-11-03 01:33:44 +0700

    add formspree

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./*.html", "./**/*.html", "./**/*.js"],
+
   theme: {
     container: {
       center: true,
